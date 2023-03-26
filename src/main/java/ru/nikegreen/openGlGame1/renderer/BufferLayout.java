9799eee3fc28717eb10a60790d9acfb53ddf8be3
nikez <nikegreen@yandex.ru>
2023-03-26 23:53:11 +0500

    добавил BufferLayout.java Облегчать работу с заполнением полей в glVertexAttribPointer(...)

--- src/main/java/ru/nikegreen/openGlGame1/renderer/BufferLayout.java
+++ src/main/java/ru/nikegreen/openGlGame1/renderer/BufferLayout.java
@@ -1,4 +1,30 @@
 package ru.nikegreen.openGlGame1.renderer;
 
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class BufferLayout {
+    @Getter
+    private List<VertexAttribute> attributes;
+
+    @Getter
+    private int stride = 0;
+
+    public BufferLayout(VertexAttribute[] attributes) {
+        this.attributes = new ArrayList<>();
+        this.attributes.addAll(Arrays.stream(attributes).toList());
+    }
+
+    public void calcOffsetAndStride() {
+        int offset = 0;
+        stride = 0;
+        for (VertexAttribute vertexAttribute: attributes) {
+            vertexAttribute.offset = offset;
+            offset += vertexAttribute.size;
+            stride += vertexAttribute.size;
+        }
+    }
 }
